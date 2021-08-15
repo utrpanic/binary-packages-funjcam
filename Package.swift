dfe1db8d82a1909572e9cfde6d3b0e3a0608de12
box-jeon <utrpanic@gmail.com>
2021-08-15 21:29:52 +0900

    Add BinaryPackages.

--- Package.swift
+++ Package.swift
@@ -9,6 +9,12 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
+            name: "BinaryPackages",
+            targets: [
+                "BinaryPackages"
+            ]
+        ),
+        .library(
             name: "SourcePackages",
             targets: [
                 "SourcePackages"
@@ -26,10 +32,10 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-//        .binaryTarget(
-//            name: "BinaryPackages",
-//            path: "BinaryPackages"
-//        ),
+        .binaryTarget(
+            name: "BinaryPackages",
+            path: "Sources/BinaryPackages/BinaryPackages.xcframework"
+        ),
         .target(
             name: "SourcePackages",
             dependencies: [
